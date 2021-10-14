e051a097b2737ab82a2d1336b5b604a11f72f62f
Adriel Hahn <adrielhahn@hotmail.com>
2021-10-14 13:29:24 -0300

    Start project

---
+++ src/main/java/com/example/aplicativo_adriel/Contato.java
@@ -0,0 +1,42 @@
+package com.example.aplicativo_adriel;
+
+public class Contato {
+    private String nome;
+    private String endereco;
+    private String empresa;
+
+    Contato(String nome, String endereco, String empresa) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.empresa = empresa;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+
+
+}
---
+++ src/main/java/com/example/aplicativo_adriel/MainActivity.java
@@ -0,0 +1,92 @@
+package com.example.aplicativo_adriel;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DBHelper dh;
+    EditText etNome, etEnd, etEmp;
+    Button btInserir, btListar;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.dh = new DBHelper(this);
+        etNome = (EditText) findViewById(R.id.etnome);
+        etEnd = (EditText) findViewById(R.id.etendereco);
+        etEmp = (EditText) findViewById(R.id.etempresa);
+
+        btInserir = (Button) findViewById(R.id.btinserir);
+        btListar = (Button) findViewById(R.id.btlistar);
+
+        btInserir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (etNome.getText().length()>0 && etEnd.getText().length()>0 && etEmp.getText().length()>0){
+                    dh.insert(etNome.getText().toString(), etEnd.getText().toString(), etEmp.getText().toString() );
+                    AlertDialog.Builder adb = new AlertDialog.Builder(MainActivity.this);
+                    adb.setTitle("Sucesso!");
+                    adb.setMessage("Cadastro Realizado!");
+                    adb.show();
+
+                    etNome.setText("");
+                    etEnd.setText("");
+                    etEmp.setText("");
+                }
+                else{
+                    AlertDialog.Builder adb = new AlertDialog.Builder(MainActivity.this);
+                    adb.setTitle("Erro!");
+                    adb.setMessage("Todos os campos devem ser prenchidos!");
+                    adb.show();
+
+                    etNome.setText("");
+                    etEnd.setText("");
+                    etEmp.setText("");
+                }
+            }
+        });
+
+        btListar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                List <Contato> contatos = dh.queryGetAll();
+                if (contatos == null){
+                    AlertDialog.Builder adb = new AlertDialog.Builder(MainActivity.this);
+                    adb.setTitle("Menssagem!");
+                    adb.setMessage("Não há registros cadastrados!");
+                    adb.show();
+
+                    return;
+                }
+                for(int i=0; i<contatos.size(); i++){
+                    Contato  contato = (Contato) contatos.get(i);
+                    AlertDialog.Builder adb = new AlertDialog.Builder(MainActivity.this);
+                    adb.setTitle("Registro " + i);
+                    adb.setMessage("Nome: " + contato.getNome() + "\nEndereço: " + contato.getEndereco() + "\nEmpresa: " + contato.getEmpresa());
+                    adb.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                        }
+                    });
+                    adb.show();
+                }
+            }
+        });
+
+    }
+}
